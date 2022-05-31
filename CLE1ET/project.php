e81a1e432d48fc94a0ffe854cff1f6373022f1ea
TracyTicar <100993717+TracyTicar@users.noreply.github.com>
2022-05-31 11:32:30 +0800

    Add files via upload

---
+++ CLE1ET/project.php
@@ -0,0 +1,40 @@
+<div id="project">
+<div id="tracy"> <h1 id="cbm"> LEARNING TASKS </h1></div>
+    <div id="divide2"> 
+        <div id="boxA">
+            <div id="namebox">
+                <h2>LT2</h2>
+                <div id="view"><h1><a href="http://uslstracy.herokuapp.com/LT2/" target="_blank" rel="noopener noreferrer">LINK HERE!</a></h1></div>
+            </div>
+            <img alt="pic" src="LT2.jpg"></div>
+        
+        
+        <div id="boxA">
+            <div id="namebox">
+                <h1>LT3</h1>
+                <div id="view"><h1><a href="http://uslstracy.herokuapp.com/LT3/" target="_blank" rel="noopener noreferrer">LINK HERE!</a></h1></div>
+            </div>
+            <img alt="pic" src="LT3.jpg">
+            
+            
+        </div>
+        <div id="boxA">
+            <div id="namebox">
+                <h1>LT4</h1>
+                <div id="view"><h1><a href="http://uslstracy.herokuapp.com/LT4/" target="_blank" rel="noopener noreferrer">LINK HERE!</a></h1></div>
+            </div>
+            <img alt="pic" src="LT4.jpg">
+            
+            
+        </div>
+        <div id="boxB"> </div>
+        <div id="boxA">
+            <div id="namebox">
+                <h1>CLE1 MIDTERM</h1>
+                <div id="view"><h1><a href="http://uslstracy.herokuapp.com/CLE1MT/" target="_blank" rel="noopener noreferrer">LINK HERE!</a></h1></div>
+            </div>
+            <img alt="pic" src="CLE1MT.jpg">
+            
+            
+        </div>
+       
